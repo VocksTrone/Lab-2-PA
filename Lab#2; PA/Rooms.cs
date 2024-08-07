d8d7a3aaf33ce20303554aa6cecf2c0ce7440ae4
VocksTrone <jr.alvarez0316@gmail.com>
2024-08-07 09:41:29 -0600

    Métodos para Mostrar la Información de las Habitaciones

--- Lab#2; PA/Rooms.cs
+++ Lab#2; PA/Rooms.cs
@@ -12,7 +12,75 @@ namespace Lab_2__PA
         public double PriceNight { get; set; }
         public bool Availability { get; set; }
         public string Status { get; set; }
-        
+        public static int ShowMenuInfo()
+        {
+            Console.WriteLine("Mostrar Información");
+            Console.WriteLine("1. Habitaciones Simples (1 - 25)");
+            Console.WriteLine("2. Habitaciones Dobles (26 - 50)");
+            Console.WriteLine("3. Suites (51 - 75)");
+            Console.WriteLine("4. Habitaciones Deluxe (76 - 100)");
+            Console.WriteLine("5. Salir");
+            Console.WriteLine("Seleccione una Opción: ");
+            int infoMenuOption = int.Parse(Console.ReadLine());
+            return infoMenuOption;
+        }
+        public static void SimpleRoomInfo(ref List<Rooms> roomsList)
+        {
+            Console.WriteLine("Habitaciones Simples");
+            foreach (SimpleRoom simpleRoom in roomsList)
+            {
+                Console.WriteLine($"Número: {simpleRoom.Number}");
+                Console.WriteLine($"Precio: {simpleRoom.PriceNight}");
+                Console.WriteLine($"Disponibilidad: {simpleRoom.Availability}");
+                Console.WriteLine($"Estado: {simpleRoom.Status}");
+                Console.WriteLine($"No. Camas: {simpleRoom.BedQuantity}");
+                Console.WriteLine("------------------------------");
+            }
+            Console.ReadKey();
+        }
+        public static void DoubleRoomInfo(ref List<Rooms> roomsList)
+        {
+            Console.WriteLine("Habitaciones Dobles");
+            foreach (DoubleRoom doubleRoom in roomsList)
+            {
+                Console.WriteLine($"Número: {doubleRoom.Number}");
+                Console.WriteLine($"Precio: {doubleRoom.PriceNight}");
+                Console.WriteLine($"Disponibilidad: {doubleRoom.Availability}");
+                Console.WriteLine($"Estado: {doubleRoom.Status}");
+                Console.WriteLine($"Vista al Mar: {doubleRoom.Seaview}");
+                Console.WriteLine("------------------------------");
+            }
+            Console.ReadKey();
+        }
+        public static void SuitesInfo(ref List<Rooms> roomsList)
+        {
+            Console.WriteLine("Suites");
+            foreach (Suite suite in roomsList)
+            {
+                Console.WriteLine($"Número: {suite.Number}");
+                Console.WriteLine($"Precio: {suite.PriceNight}");
+                Console.WriteLine($"Disponibilidad: {suite.Availability}");
+                Console.WriteLine($"Estado: {suite.Status}");
+                Console.WriteLine($"Número de Habitaciones: {suite.NumberRooms}");
+                Console.WriteLine($"Jacuzzi: {suite.Jacuzzi}");
+                Console.WriteLine("------------------------------");
+            }
+            Console.ReadKey();
+        }
+        public static void DeluxeRoom(ref List<Rooms> roomsList)
+        {
+            Console.WriteLine("Habitaciones Deluxe");
+            foreach (DeluxeRoom deluxeRoom in roomsList)
+            {
+                Console.WriteLine($"Número: {deluxeRoom.Number}");
+                Console.WriteLine($"Precio: {deluxeRoom.PriceNight}");
+                Console.WriteLine($"Disponibilidad: {deluxeRoom.Availability}");
+                Console.WriteLine($"Estado: {deluxeRoom.Status}");
+                Console.WriteLine($"Servicios Extras: {deluxeRoom.ExtraServices}");
+                Console.WriteLine("------------------------------");
+            }
+            Console.ReadKey();
+        }
         public Rooms(int number, double pricenight, bool availability, string status)
         {
             Number = number;
